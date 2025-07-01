1cc4bc48d2bc9f1f1191f6122073d78df7ed9506
yjh <langbiantianya@outlook.com>
2025-07-01 16:08:52 +0800

    feat: 优化模板

--- assets/gradle/web/3/build.gradle.kts
+++ assets/gradle/web/3/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot").version("3.5.0")
     id("io.spring.dependency-management").version("1.1.7")
     id("java")
+    kotlin("jvm") version "2.2.0"
 }
 
 group = "com.sensorsdata"
