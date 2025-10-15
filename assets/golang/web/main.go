e3133a851ead940d00c254d8aa2e7a0feb18575f
lbty <langbiantianya@hotmail.com>
2025-10-15 14:33:00 +0800

    fix: bug

--- assets/golang/web/main.go
+++ assets/golang/web/main.go
@@ -7,6 +7,7 @@ import (
 	"{{.ModuleName}}/middleware"
 	"{{.ModuleName}}/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
@@ -15,9 +16,8 @@ func main() {
 	gin.SetMode(config.Conf.Server.Mode)
 	r := gin.Default()
 	r.Use(middleware.Log())
-	r.Use(middleware.Cors(middleware.DefaultCorsConfig()))
+	r.Use(cors.Default())
 	router.RegisterRouter(r)
-	config.PrintInfo()
 	addr := fmt.Sprintf("%s:%d", config.Conf.Server.Host, config.Conf.Server.Port)
 	if config.Conf.Server.Mode == gin.ReleaseMode {
 		logrus.Infof("Listening and serving HTTP on %s", addr)
--- assets/golang/web/router/router.go
+++ assets/golang/web/router/router.go
@@ -1,10 +1,19 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"{{.ModuleName}}/public"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRouter(r *gin.Engine) {
-	// 静态资源路由
-	r.Static("/static", "./static")
+	fs, err := static.EmbedFolder(public.StaticFs, "static")
+	if err != nil {
+		panic(err)
+	}
+	r.Use(static.Serve("/static", fs))
+
 	// API 路由组（版本控制）
 	api := r.Group("/api/v1")
 
